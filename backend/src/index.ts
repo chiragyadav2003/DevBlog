4149f855b4ce73fe833746a514e589075e26cfb8
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-03-13 18:27:49 +0530

    Add API routes for user signup, signin, blog creation, update, and retrieval

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,35 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.get('/api/v1/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.post("/api/v1/user/signup", (c)=>{
+  return c.text('signup route')
+})
+
+app.post("/api/v1/user/signin", (c)=>{
+  return c.text('signin route')
+})
+
+app.post("/api/v1/blog", (c)=>{
+  return c.text('blog!')
+})
+
+app.put("/api/v1/blog", (c)=>{
+  return c.text('blog update route')
+})
+
+
+app.get("/api/v1/blog/:id", (c)=>{
+  const id = c.req.param('id')
+  console.log(id)
+  return c.text('get blog route!')
+})
+
+app.get("/api/v1/blog/bulk", (c)=>{
+  return c.text('signin route - get all blogs')
 })
 
 export default app
