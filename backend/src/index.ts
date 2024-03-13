646ee142ae3e6b01c82c23d4e7d67e0b6f992a71
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-03-13 19:29:18 +0530

    Initialize DB (prisma) and setting up connection pooling

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,19 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { env } from 'hono/adapter'
 
 const app = new Hono()
 
 app.get('/api/v1/', (c) => {
+
+  // here DATABASE_URL accessed from wrangler.toml file
+	const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c)
+
+	const prisma = new PrismaClient({
+		datasourceUrl: DATABASE_URL,
+	}).$extends(withAccelerate())
+
   return c.text('Hello Hono!')
 })
 
